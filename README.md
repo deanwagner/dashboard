a693b509871a9b3857df1365fc48aeeefaaf9498
Dean Wagner <me@deanwagner.net>
2022-02-16 21:53:39 -0800

    tidy up and readme edit

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # Dashboard
-### Admin Dashboard Proof of Concept
+### Admin Dashboard Project
 
 Live Demo:  
 https://deanwagner.github.io/dashboard/
+
+![Project Screenshot](https://deanwagner.github.io/dashboard/img/dashboard-pc-light.png)
+
+![Project Screenshot](https://deanwagner.github.io/dashboard/img/dashboard-pc-dark.png)
+
+
+![Project Screenshot](https://deanwagner.github.io/dashboard/img/dashboard-mobile-light.png) ![Project Screenshot](https://deanwagner.github.io/dashboard/img/dashboard-mobile-dark.png)
+
+This project was created for the [Admin Dashboard assignment](https://www.theodinproject.com/paths/full-stack-javascript/courses/intermediate-html-and-css/lessons/admin-dashboard) as part of [The Odin Project](https://www.theodinproject.com) curriculum. I met all the assignment objectives and then expanded on it with my own concepts to make it more functional and user-friendly.
+
+### Provided Assets
+
+* Project Design File: [dashboard-project.png](https://cdn.statically.io/gh/TheOdinProject/curriculum/main/html_css/grid-lessons/project-dashboard/dashboard-project.png)
+* SVG Icon Library: [Material Design Icons](https://materialdesignicons.com/)
+
+### Objectives
+
+1. Build a page based on the Provided Assets
+2. (+1) Make it Responsive
+
+### Scope Creep
+
+* Light/Dark themes that automatically load based on the user's browser/OS settings
+* Search Hints `<datalist>` populated with page links
--- res/dashboard.js
+++ res/dashboard.js
@@ -1,5 +1,16 @@
 "use strict";
 
+/**
+ * Dashboard
+ * @class
+ * @property {array}  light     - CSS Properties for Light Mode
+ * @property {array}  dark      - CSS Properties for Dark Mode
+ * @property {object} lightMode - Enable Light Mode Element
+ * @property {object} darkMode  - Enable Dark Mode Element
+ * @property {array}  projects  - JSON Array of Projects
+ * @property {array}  exercises - JSON Array of Exercises
+ * @author Dean Wagner <info@deanwagner.net>
+ */
 class Dashboard {
 
     // Class Properties
@@ -10,6 +21,7 @@ class Dashboard {
      *    Projects     *
     \* * * * * * * * * */
 
+    // Projects
     projects = [{
         name : 'LCD Calculator',
         desc : 'An LCD Calculator App written in JavaScript with an SVG display.',
@@ -72,6 +84,7 @@ class Dashboard {
      *    Exercises    *
     \* * * * * * * * * */
 
+    // Exercises
     exercises = [{
         name : 'Caesar Cipher',
         desc : 'Encrypt/Decrypt messages using the Caesar Cipher.',
@@ -193,15 +206,21 @@ class Dashboard {
         type : 'css'
     }];
 
+    /**
+     * Constructor
+     * @constructor
+     */
     constructor() {
 
         /* * * * * * * * * *\
          * Configure Theme *
         \* * * * * * * * * */
 
+        // Class Elements
         this.darkMode  = document.getElementById('dark_mode');
         this.lightMode = document.getElementById('light_mode');
 
+        // CSS Properties
         const keys = [
             'accent-color',
             'special-color',
@@ -220,11 +239,13 @@ class Dashboard {
             'action-text-glow'
         ];
 
+        // Get Values from CSS
         keys.forEach(value => {
             this.light[value] = getComputedStyle(document.documentElement).getPropertyValue('--' + value);
             this.dark[value]  = getComputedStyle(document.documentElement).getPropertyValue('--dark-' + value);
         });
 
+        // Auto-Load Dark Mode per User Settings
         const isDark = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--dark-mode'));
         if (isDark) {
             this.darkMode.classList.add('off');
@@ -239,9 +260,9 @@ class Dashboard {
 
         // Projects
         const container = document.createElement('div');
-        this.projects.forEach(p => {
+        this.projects.forEach(project => {
             const article = document.createElement('article');
-            article.innerHTML = this.buildProject(p);
+            article.innerHTML = this.buildProject(project);
             container.appendChild(article);
         });
         document.getElementById('primary').appendChild(container);
@@ -251,20 +272,33 @@ class Dashboard {
         const stylesheets = document.getElementById('stylesheets');
         const js          = document.createElement('article');
         const css         = document.createElement('article');
-        this.exercises.forEach(e => {
-            if (e.type === 'js') {
-                js.innerHTML += this.buildExercise(e);
+        this.exercises.forEach(exercise => {
+            if (exercise.type === 'js') {
+                js.innerHTML += this.buildExercise(exercise);
             } else {
-                css.innerHTML += this.buildExercise(e);
+                css.innerHTML += this.buildExercise(exercise);
             }
         });
         javascript.appendChild(js);
         stylesheets.appendChild(css);
 
+        // Build Search Hints Datalist from page links
+        const datalist = document.getElementById('search-hints');
+        const links = document.getElementsByTagName('a');
+        for (let i = 0; i < links.length; i++) {
+            const text = links[i].textContent.trim();
+            if (text !== '') {
+                const option = document.createElement('option');
+                option.value = text;
+                datalist.appendChild(option);
+            }
+        }
+
         /* * * * * * * * * *\
          * Event Listeners *
         \* * * * * * * * * */
 
+        // Dark Mode Button
         this.darkMode.addEventListener('click', () => {
             this.darkMode.classList.add('off');
             this.lightMode.classList.remove('off');
@@ -272,6 +306,7 @@ class Dashboard {
             this.changeMode(this.dark);
         });
 
+        // Light Mode Button
         this.lightMode.addEventListener('click', () => {
             this.lightMode.classList.add('off');
             this.darkMode.classList.remove('off');
@@ -279,6 +314,7 @@ class Dashboard {
             this.changeMode(this.light);
         });
 
+        // Resize Window Event Listener
         const aside = document.getElementsByTagName('aside')[0];
         window.addEventListener('resize', () => {
             if (window.innerHeight >= window.innerWidth) {
@@ -288,37 +324,36 @@ class Dashboard {
             }
         });
 
+        // Expand/Collapse Navigation
         const h1 = document.getElementsByTagName('h1')[0];
         h1.addEventListener('click', () => {
             aside.classList.toggle('collapsed');
         });
 
+        // Automatically Collapse Nav for Mobile Devices
         const isMobile = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--mobile-view'));
         if (isMobile) {
             aside.classList.add('collapsed');
         }
-
-        const datalist = document.getElementById('search-hints');
-        const links = document.getElementsByTagName('a');
-        for (let i = 0; i < links.length; i++) {
-            const option = document.createElement('option');
-            option.value = links[i].textContent.trim();
-            datalist.appendChild(option);
-        }
     }
 
-    buildProject(p) {
+    /**
+     * Project HTML Template
+     * @param {object} project - Project Object
+     * @returns {string} - HTML String
+     */
+    buildProject(project) {
         return `
-            <h4><a href="${p.live}"><img src="${p.icon}" alt="Project Icon" loading="lazy">${p.name}</a></h4>
-            <img src="${p.img}" alt="Thumbnail Image" loading="lazy">
-            <p>${p.desc}</p>
+            <h4><a href="${project.live}"><img src="${project.icon}" alt="Project Icon" loading="lazy">${project.name}</a></h4>
+            <img src="${project.img}" alt="Thumbnail Image" loading="lazy">
+            <p>${project.desc}</p>
             <div>
-                <a title="Live Version" href="${p.live}">
+                <a title="Live Version" href="${project.live}">
                     <svg class="icon_view" viewBox="0 0 24 24">
                         <path d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z" />
                     </svg>
                 </a>
-                <a title="GitHub Repository" href="${p.repo}">
+                <a title="GitHub Repository" href="${project.repo}">
                     <svg class="icon_github" viewBox="0 0 32 32">
                         <path d="M16.0018,0A16.2088,16.2088,0,0,0,0,16.4223,16.4156,16.4156,0,0,0,10.7179,31.9376c.84.2166.7114-.3966.7114-.8153v-2.846c-4.8513.5839-5.0479-2.7138-5.3732-3.2646-.6578-1.1531-2.213-1.4469-1.7482-1.9977,1.1047-.5839,2.2308.1469,3.5357,2.1262.9438,1.4359,2.7849,1.1935,3.718.9548a4.664,4.664,0,0,1,1.24-2.2327c-5.0264-.9254-7.1214-4.0762-7.1214-7.8219A7.3467,7.3467,0,0,1,7.4074,11.204a6.5675,6.5675,0,0,1,.1752-4.4067C9.66,6.6064,11.819,8.325,11.987,8.4609a15.14,15.14,0,0,1,4.0362-.5,15.1577,15.1577,0,0,1,4.0577.51c.404-.3158,2.406-1.7921,4.3365-1.6121a6.6124,6.6124,0,0,1,.1966,4.3332,7.3488,7.3488,0,0,1,1.7482,4.8584c0,3.7531-2.1093,6.9075-7.15,7.8182a4.7469,4.7469,0,0,1,1.362,3.3418v4.1313c.0286.3305,0,.6573.5363.6573A16.4082,16.4082,0,0,0,32,16.426,16.2136,16.2136,0,0,0,16.0018,0Z" />
                     </svg>
@@ -327,13 +362,22 @@ class Dashboard {
         `;
     }
 
-    buildExercise(e) {
+    /**
+     * Exercise HTML Template
+     * @param {object} exercise - Exercise Object
+     * @returns {string} - HTML String
+     */
+    buildExercise(exercise) {
         return `
-            <h4><a href="${e.link}">${e.name}</a></h4>
-            <p>${e.desc}</p>
+            <h4><a href="${exercise.link}">${exercise.name}</a></h4>
+            <p>${exercise.desc}</p>
         `;
     }
 
+    /**
+     * Change Light/Dark Mode
+     * @param {object} colors - [this.light] or [this.dark]
+     */
     changeMode(colors) {
         for (let index in colors) {
             document.documentElement.style.setProperty('--' + index, colors[index]);
